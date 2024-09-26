32392a991139fa7c5e8b7f943480dfc2da18fcd9
manojkumar5251 <manojkumar5251@gmail.com>
2024-09-26 14:12:55 +0530

    Function supporting specific delimitor

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,15 @@
 export const add = (numbers = "") => {
   let sum = 0;
-  numbers.split(/,|\n/g).forEach((el) => {
+  let delimitor = ",|\n";
+  const isDynamicDelimitor = numbers.indexOf("//") !== -1;
+
+  if (isDynamicDelimitor) {
+    const endIndex = numbers.indexOf("\n");
+    delimitor = numbers.slice(2, endIndex);
+    numbers = numbers.slice(endIndex);
+  }
+
+  numbers.split(new RegExp(delimitor, "g")).forEach((el) => {
     sum = sum + Number(el);
   });
 
