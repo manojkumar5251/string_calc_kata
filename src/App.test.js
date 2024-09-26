0bb26c3b9dac20069caeddfa171af4ed7dafabfd
manojkumar5251 <manojkumar5251@gmail.com>
2024-09-26 15:49:12 +0530

    Added test case to check the calculation

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { add } from "./utils";
 import App from "./App";
 
 describe("User interface for the String Calculator", () => {
   test("renders the text input", () => {
     render(<App />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
   });
 
   test("renders the button", () => {
     render(<App />);
-    const buttonElement = screen.getByRole("calc");
-    expect(buttonElement).toBeInTheDocument();
+    const button = screen.getByRole("calc");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("calculates the data on button click", () => {
+    render(<App />);
+
+    const input = screen.getByRole("textbox");
+    const button = screen.getByRole("calc");
+
+    fireEvent.change(input, { target: { value: "1,3" } });
+    fireEvent.click(button);
+
+    const result = screen.getByRole("result");
+
+    expect(result).toHaveValue(add("4"));
   });
 });
